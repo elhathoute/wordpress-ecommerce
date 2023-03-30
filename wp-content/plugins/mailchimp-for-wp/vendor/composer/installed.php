222785e958ddc6777c770f2aa3bfd3249aeda730
abdelaziz <abdelaazizeelhathoute.2018@gmail.com>
2023-03-30 12:04:42 +0000

    add categories

---
+++ wp-content/plugins/mailchimp-for-wp/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '847dd292fd5be420d90edbbb7f35aa0a1343e3b2',
+        'name' => 'ibericode/mailchimp-for-wordpress',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'ibericode/mailchimp-for-wordpress' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '847dd292fd5be420d90edbbb7f35aa0a1343e3b2',
+            'dev_requirement' => false,
+        ),
+    ),
+);
